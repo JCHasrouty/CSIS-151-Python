1c156d194d620469f13dc8608723af3d23bf0202
Jean Claude Hasrouty <jchasrouty@Jeans-MacBook-Pro.local>
2018-04-26 21:30:07 -0700

    cleaned up lab 7 files

--- Ch7Lab1.py
+++ Ch7Lab1.py
@@ -1,63 +1,41 @@
 ROWS = 2
 COLS = 7
-
 def main():
-    week_sales = [["Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
-                  [0.0,0.0,0.0,0.0,0.0,0.0,0.0]]
-
-    filename = ask_sales(week_sales)
-    sales_price = ask_sales_again()
-
-    if filename.upper() != "QUIT":
-        readContents(filename)
-
-def ask_sales_again():
-    new_sale = float(input("Invalid sales value.\n"))
-    return new_sale
-
-def ask_sales(sales_list):
-    total = 0
-    for c in range(COLS):
-        print("Please enter sales for", sales_list[0][c], ":")
-        sales_list[1][c] = float(input())
-        if sales_list[1][c] < 0:
-            ask_sales_again()
-        else:
-            total += sales_list[1][c]
-    # need to add input validation in here
-    return sales_list
-
-def file_exists(filename):
-    try:
-        file = open(filename)
-        file.close()
-    except FileNotFoundError:
-        file_ok = False
-    else:
-        file_ok = True
-        
-    return file_ok
-
-def readContents(filename):
-    file = open(filename, 'r')
-    count = 0
-    totalSum = 0
-    # count amount of lines extracted from file
-    line = file.readline()
-    if line == '':
-        print("File", filename, "is empty.")
-        return
-    else:
-        while line != '':
-            file_info = float(line)
-            totalSum += file_info
-            count += 1
-            
-            line = file.readline()
-
-    average = totalSum / count
-    print("Total:", format(totalSum, "2.3f"))
-    print("Average:", format(average, "2.3f"))
-    file.close()
+    week_sales = [["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+                  [0.0] * COLS]
     
+    calculate_sales(week_sales)
+
+def calculate_sales(sales_list):
+    total_sales = 0
+    avg_sales = 0
+    size = len(sales_list[0])
+    for c in range(COLS):
+        print("Please enter sales for ", sales_list[0][c], ": ", sep='', end='') 
+        sale_price = input()
+
+        while not isValid(sale_price):
+            print("Invalid sales value.")
+            print("Please enter sales for ", sales_list[0][c], ": ", sep='', end='')
+            sale_price = input()
+        sales_list[1][c] = float(sale_price)
+        total_sales += sales_list[1][c]
+    avg_sales = total_sales / size
+    print("Total sales: ", "$", format(total_sales, ".2f"), sep='')
+    print("Average sales: ", "$", format(avg_sales, ".2f"), sep='')
+
+def isValid(num):
+        valid = True
+        if not num.isdigit():
+            valid = False
+            return valid
+        
+        new_num = int(num)
+
+        if new_num < 0:
+            valid = False 
+            return valid
+        else:
+            valid = True
+            return valid
 main()
