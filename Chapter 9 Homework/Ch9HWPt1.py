cb80aa71f7b08c8fc007ae3789f388c9addeedea
Jean Claude Hasrouty <jchasrouty@Jeans-MacBook-Pro.local>
2018-05-16 18:58:53 -0700

    Began working on a dictionary type of database to store employees

--- Chapter 9 Homework/Ch9HWPt1.py
+++ Chapter 9 Homework/Ch9HWPt1.py
@@ -1,12 +1,103 @@
 ## chapter 9 homework 
 ## make a dictionary with the key being the employeee name and the value a list
 ## value is a dictionary also but will contain EID, dept, salary etc
-## 
-##
-##
-##
-##
-##
-##
-##
-##
+
+# Global constants for menu choices
+
+ADD_EMP = 1
+FIND_BN = 2
+FIND_BY_EID = 3
+DEL_EMP = 4
+DISP_STATS = 5
+DISP_EMP = 6
+QUIT = 7
+
+def main():
+    ## initialize empty dictionary
+    employees = {}
+    user_input = 0
+
+    while user_input != QUIT:
+        # display menu
+        user_input = get_menu_choice()
+
+        if user_input == ADD_EMP:
+            addEmployee(employees)
+            
+        elif user_input == FIND_BN:
+            findEmpByName(employees)
+            
+        elif user_input == FIND_BY_EID:
+            findEmpByEID(employees)
+            
+        elif user_input == DEL_EMP:
+            deleteEmployee(employees)
+            
+        elif user_input == DISP_STATS:
+            dispStats(employees)
+            
+        elif user_input == DISP_EMP:
+            dispEmployees(employees)
+            
+        ## for debugging purposes
+        ## print(employees)
+
+def get_menu_choice():
+    print("Employee Database")
+    print("-----------------")
+    print("1. Add an Employee")
+    print("2. Find an Employee (By Name)")
+    print("3. Find an Employee (By EID)")
+    print("4. Delete an Employee")
+    print("5. Display Statistics")
+    print("6. Display All Employees")
+    print("7. Exit")
+    print()
+
+    user_input = int(input("Please enter your choice: "))
+
+    while user_input < ADD_EMP or user_input > QUIT:
+        user_input = int(input("Please enter a valid choice: "))
+
+    return user_input
+    
+def addEmployee(dictionary):
+    new_emp = input("Please enter the employees name or type quit to cancel: ")
+    emp_info = input("Please enter the employees ID, Department, Title, and Salary: ")
+## While adding an employee you should allow the user to either cancel the operation
+## or re-enter the name again.
+    if new_emp.upper() != "QUIT":
+        if new_emp not in dictionary:
+            dictionary[new_emp] = emp_info
+        else:
+            print("Employee employee already exists.")
+    print()
+        
+def findEmpByName(dictionary):
+    # lowercase entire word then capitalize first letter and search
+    emp_to_find = input("Please enter the name of the employee you'd like to find: ")
+    emp_insensitive = emp_to_find.lower().capitalize()
+    print(emp_insensitive)
+    print(dictionary.get(emp_insensitive, "Not found."))
+    print()
+          
+def findEmpByEID(dictionary):
+    print("This function is functional but not yet implemented... stay tuned.") 
+def deleteEmployee(dictionary):
+    emp_name = input("Enter an employee name: ")
+
+    if emp_name in dictionary:
+        del dictionary[emp_name]
+    else:
+        print("That employee was not found.")
+    print()
+        
+def dispStats(dictionary):
+    print("This function is functional but not yet implemented... stay tuned.")
+    
+def dispEmployees(dictionary):
+    for key in dictionary:
+        print(key, dictionary[key])
+    print()
+    
+main()
